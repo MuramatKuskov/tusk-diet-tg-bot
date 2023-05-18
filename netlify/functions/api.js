99aaa034eaf0a1469015de69699c23aa88ad59f9
Kuskov <ki6org1488@gmail.com>
2023-05-18 16:36:19 +0300

    Backend init

---
+++ netlify/functions/api.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+// middleware парсить жсон
+app.use(express.json());
+// mw для кроссдоменных запросов (для облачного бэка)
+app.use(cors());
+// обработка входящего запроса
+app.post('/web-data', async (req, res) => {
+	const { queryId, newRecipe } = req.body;
+	// dataBase.push(newRecipe)
+	try {
+		await bot.answerWebAppQuery(queryId, {
+			type: 'article',
+			id: queryId,
+			title: 'Успех',
+			input_message_content: { message_text: 'Рецепт добавлен' }
+		});
+		return res.status(200).json({});
+	} catch (error) {
+		await bot.answerWebAppQuery(queryId, {
+			type: 'article',
+			id: queryId,
+			title: 'Неудача',
+			input_message_content: { message_text: 'Не удалось добавить рецепт: ' + error }
+		});
+		return res.status(500).json({});
+	}
+})
+
+const PORT = 8080;
+app.listen(PORT, () => { console.log('Server started!'); });
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 // сторонние пакеты
 const TelegramBot = require('node-telegram-bot-api');
-const express = require('express');
-const cors = require('cors');
+
 // свои импорты
 const createRecipe = require('./modules/createRecipe');
 
@@ -10,13 +9,7 @@ const channelID = process.env.channelID;
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true });
-console.log(token, bot.token);
-const app = express();
 
-// middleware парсить жсон
-app.use(express.json());
-// mw для кроссдоменных запросов (для облачного бэка)
-app.use(cors());
 
 async function start() {
 	bot.on("message", async (msg) => {
@@ -51,30 +44,6 @@ async function start() {
 	})
 }
 
-// обработка входящего запроса
-app.post('/web-data', async (req, res) => {
-	const { queryId, newRecipe } = req.body;
-	// dataBase.push(newRecipe)
-	try {
-		await bot.answerWebAppQuery(queryId, {
-			type: 'article',
-			id: queryId,
-			title: 'Успех',
-			input_message_content: { message_text: 'Рецепт добавлен' }
-		});
-		return res.status(200).json({});
-	} catch (error) {
-		await bot.answerWebAppQuery(queryId, {
-			type: 'article',
-			id: queryId,
-			title: 'Неудача',
-			input_message_content: { message_text: 'Не удалось добавить рецепт: ' + error }
-		});
-		return res.status(500).json({});
-	}
-})
+
 
 start();
-
-const PORT = 8080;
-app.listen(PORT, () => { console.log('Server started!'); });
